f0e278a12d8bfcb8b13a3cd69189170ec12299d1
x09sa <75315110+x09sa@users.noreply.github.com>
2024-04-09 02:42:29 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,29 +16,7 @@ function Header() {
               </p>
             </div>
 
-            <div class="flex flex-col gap-4 sm:mt-0 justify-center sm:flex-row">
-              <button
-                class=" rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring sm:w-50"
-                type="button"
-              >
-                <a
-                  class="text-sm font-medium"
-                  href="https://ali-hatem-ramadan.vercel.app/"
-                  target="_blank"
-                >
-                  View my profile
-                </a>
-                <i class="ml-2 text-sm fas fa-external-link-alt"></i>
-              </button>
-
-              <div class="w-12 h-12 rounded-full overflow-hidden border-2 border-gray-200">
-                <img
-                  src="https://i.postimg.cc/Z57TPpyP/400108500287-305640-01-01.jpg"
-                  alt="random"
-                  className="rounded-full w-full h-full object-cover"
-                />
-              </div>
-            </div>
+          
           </div>
         </div>
       </header>
